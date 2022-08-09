c685fbeaa41b654cb3291dba1a9ee315ae865a8e
AndrewDar <geologalone@gmail.com>
2022-08-09 22:23:35 +0300

    added condition for two-one-digit numbers

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -9,26 +9,33 @@
 
 
 int ThirdDigit(int num)
+{   
+    int count = 0;
+    int num1 = num;
+    while(num1>=10)
 {
-    
-int count = 0;
-int num1 = num;
-while(num1>=10)
-{
-    num1 = num1/10;
-    count++;
+        num1 = num1/10;
+        count++;
 }
-int degree = count-2;
-int newNum = num; 
-while (degree>0)
+    int degree = count-2;
+    int num2 = num; 
+    while (degree>0)
 {
-    newNum = newNum/10;
-    degree = degree-1;
+        num2= num2/10;
+        degree = degree-1;
 }
-int thirdDigit = newNum%10;
+    int thirdDigit = num2%10;
     return thirdDigit;
 }
+
 Console.Write("Enter number: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int result = ThirdDigit(number);
-Console.WriteLine($"The third digit of number {number} is {result}");
+if (number<100)
+{
+    Console.WriteLine("The third digit doesn't exist");
+}
+else 
+{
+    int result = ThirdDigit(number);
+    Console.WriteLine($"The third digit of number {number} is {result}");
+}
